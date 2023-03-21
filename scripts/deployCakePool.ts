69f282a4b18231c08475e15b174ac48689b469a9
Paulo <pauloxti@gmail.com>
2023-03-21 21:08:11 +0100

    deployed new version

--- scripts/deployCakePool.ts
+++ scripts/deployCakePool.ts
@@ -19,8 +19,8 @@ async function main() {
     );
     console.log();
 
-    const SFYX = "0x4a364546B6765a3469ab131b96ddEbe4A2199082"
-    const MasterChefV2 = "0x5Fe1C14b7C07E448127D4815e588A946c4C42Be2"
+    const SFYX = "0xab738F29E55DF25D69006AAEFb638307b9b0ED3E"
+    const MasterChefV2 = "0x8024c5299c301fbB87d2e6Df6f4F6bdF7B80B9C8"
     const _burnAdmin = deployer.address;
     const _treasury = "0x334e0B1c6031EB348DDfABd60C45b5ff8Cb8f6b9";
     const _operator = "0x334e0B1c6031EB348DDfABd60C45b5ff8Cb8f6b9";
